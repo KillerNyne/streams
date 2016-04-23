885fb2fd3795b4793df33b3b37e173a060e83895
killernyne <mstith@leaders6-12.org>
2016-04-23 17:04:38 +0000

    Restored to session 16 start

---
+++ routes.js
@@ -0,0 +1,21 @@
+// Local deps
+var controllers = require('./controllers');
+
+var Router = {
+
+    init: function(app) {
+
+        // Home page
+        app.get('/', controllers.Home.index);
+        
+        // About page
+        app.get('/about', controllers.About.index);
+
+        // Users
+        app.get('/users', controllers.Users.index);
+
+    }
+
+};
+
+module.exports = Router;

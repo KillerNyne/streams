6efcad49eb82d078686dddedd1744055b890c2ce
killernyne <mstith@leaders6-12.org>
2016-05-07 19:00:41 +0000

    deleted users files

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
 // Local deps
 var About = require('./about');
 var Home = require('./home');
-var Users = require('./users');
 var Search = require('./search')
 module.exports = {
     About: About,
     Home: Home,
-    Users: Users,
     Search: Search
 };
--- controllers/search.js
+++ controllers/search.js
@@ -1,7 +1,7 @@
 var Search = {
-    index: function(req,res){
+    index: function(req, res){
     res.render ('search/index');
 }
-};//Search controller 
+    };
 
 module.exports = Search;
